f73f432c7182e91483b644865b1f07f5de82a413
Abel Palomino <abelpr87@gmail.com>
2021-09-04 17:00:39 -0500

    Start project

---
+++ src/java/models/Categoria.java
@@ -0,0 +1,54 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package models;
+
+/**
+ *
+ * @author Abel
+ */
+public class Categoria {
+    //Variables / Atributos
+    private int idCategoria;
+    private String nombre;
+    private String descripcion;
+
+    //Constructores
+    public Categoria() {
+    }
+
+    public Categoria(int idCategoria, String nombre, String descripcion) {
+        this.idCategoria = idCategoria;
+        this.nombre = nombre;
+        this.descripcion = descripcion;
+    }
+
+    //Metodos de Acceso
+    public int getIdCategoria() {
+        return idCategoria;
+    }
+
+    public void setIdCategoria(int idCategoria) {
+        this.idCategoria = idCategoria;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    
+}
